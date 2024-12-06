1cb1be9f4536782e6c810457e078183bd1105119
zhuolugmail <zhuolu@gmail.com>
2024-12-05 19:25:53 -0600

    change to next14 and update workspace

--- ai-app/tailwind.config.ts
+++ ai-app/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,4 +15,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
